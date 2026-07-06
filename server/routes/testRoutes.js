720b8fd065f3be6c0e47dc975b8ddb40c1bedaaf
DineshKumarSahoo-2005 <dineshkumarsahoo30776@gmail.com>
2026-07-06 21:29:36 +0530

    Day 2: Setup Express server, MongoDB connection, and first API

---
+++ server/routes/testRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/ping", (req, res) => {
+    res.json({
+        success: true,
+        message: "API is working!"
+    });
+});
+
+export default router;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import testRoutes from "./routes/testRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/test", testRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Smart Restaurant API is Running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
